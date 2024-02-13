3abbf5e5652fc51226dc7bc72b3630c482910fe5
iamutk4 <utkarsh_jadon@hotmail.com>
2024-02-12 20:01:02 -0500

    updated title

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ const App = () => {
 
     return (
         <div className="app">
-            <h1>MovieLand</h1>
+            <h1>MovieHub</h1>
 
             <div className="search">
                 <input
